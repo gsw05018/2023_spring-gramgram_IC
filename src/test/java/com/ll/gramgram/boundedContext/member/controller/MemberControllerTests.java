b6f608de32fdae88e0e7f87dc352dc1212f4d12b
한인철 <gsw05018@naver.com>
2023-10-24 21:15:23 +0900

    join.html tailwind / jointest

--- src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.boundedContext.member.controller;
 
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +11,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest // 스프링부트 관련 컴포넌트 테스트할 때 붙여야 함, Ioc 컨테이너 작동시킴
 @AutoConfigureMockMvc // http 요청, 응답 테스트
@@ -23,21 +24,30 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test") // application-test.yml 을 활성화 시킨다.
 public class MemberControllerTests {
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mvc; // MockMvc 객체 주입받음
 
     @Test
-    @DisplayName("회원가입 폼")
+    @DisplayName("회원가입 폼") // 테스트 메서드에 대한 설명을 나타내는 애너테이션
     void t001() throws Exception {
         // WHEN
         ResultActions resultActions = mvc
-                .perform(get("/member/join"))
+                .perform(get("/member/join")) // member/join으로 get요청 수행
                 .andDo(print()); // 크게 의미 없고, 그냥 확인용
 
         // THEN
         resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("showJoin"))
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(handler().handlerType(MemberController.class)) // 핸들러의 타입 확인
+                .andExpect(handler().methodName("showJoin")) // 핸들러의 메서드 이름 확인
+                .andExpect(status().is2xxSuccessful()) // HTTP 응답 상태가 2xx성공인지 확인
+                .andExpect(content().string(containsString(""" 
+                        <input type="text" name="username"
+                        """.stripIndent().trim()))) // 응답 본문에 특정 문자열이 포함되어 있는지 확인
+                .andExpect(content().string(containsString(""" 
+                        <input type="password" name="password"
+                        """.stripIndent().trim())))
+                .andExpect(content().string(containsString("""
+                        <input type="submit" value="회원가입"
+                        """.stripIndent().trim())));
     }
 
     @Test
@@ -45,17 +55,17 @@ public class MemberControllerTests {
     void t002() throws Exception {
         // WHEN
         ResultActions resultActions = mvc
-                .perform(post("/member/join")
-                        .with(csrf()) // CSRF 키 생성
-                        .param("username", "user10")
-                        .param("password", "1234")
+                .perform(post("/member/join") // member/join으로 post 요청 수행
+                        .with(csrf()) // CSRF 키 생성 
+                        .param("username", "user10") // 요청에 username 파라미터 추가
+                        .param("password", "1234") // 요청에 password 파라미터 추가
                 )
                 .andDo(print());
 
         // THEN
-        resultActions
-                .andExpect(handler().handlerType(MemberController.class))
-                .andExpect(handler().methodName("join"))
-                .andExpect(status().is3xxRedirection());
+        resultActions 
+                .andExpect(handler().handlerType(MemberController.class)) // 핸들러의 타입 확인
+                .andExpect(handler().methodName("join")) // 핸들러의 메서드 이름 확인
+                .andExpect(status().is3xxRedirection()); // HTTP 응답 상태가 3xx 리다이렉션인지 확인
     }
 }
